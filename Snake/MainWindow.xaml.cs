0ee511e501b568f979f0ed08c32e45f0d90dc445
Munakotka <164500840+nkahkone@users.noreply.github.com>
2025-11-02 18:43:43 +0200

    resized the game grid

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace Snake
             { Direction.Left, 270 },
         };
 
-        private readonly int rows = 15, cols = 15;
+        private readonly int rows = 20, cols = 20;
         private readonly Image[,] gridImages;
         private GameState gameState;
         private bool gameRunning;

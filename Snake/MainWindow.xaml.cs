d87d3f71f7f259e6e4a9000f84b182195e0e07ce
Munakotka <164500840+nkahkone@users.noreply.github.com>
2025-11-02 17:05:44 +0200

    added a game over screen and ability to restart the game

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -41,6 +41,8 @@ namespace Snake
             await ShowCountDown();
             Overlay.Visibility = Visibility.Hidden;
             await GameLoop();
+            await ShowGameOver();
+            gameState = new GameState(rows, cols);
         }
 
         private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
@@ -146,6 +148,13 @@ namespace Snake
                 OverlayText.Text = i.ToString();
                 await Task.Delay(500);
             }
+        }
+
+        private async Task ShowGameOver()
+        {
+            await Task.Delay(1000);
+            Overlay.Visibility = Visibility.Visible;
+            OverlayText.Text = "PRESS ANY KEY TO START";
         }
     }
 }
